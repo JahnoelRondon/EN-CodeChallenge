822c6daebca98988264471bc9251b781b36a0581
JahnoelRondon <jahnoelrondon03@gmail.com>
2022-03-14 14:22:38 -0400

    added routes and controller folders and started restful routing

--- README.md
+++ README.md
@@ -1,7 +1,13 @@
 ## Steps
 
+1. setting up the backend
+
 create react app
 
-install nodemon & express
+install nodemon, express, mongoose, dotenv
 
-create beginning routes in app
+create beginning routes and middleware in app
+
+connect to database
+
+Routes and controller and begin restful routing
--- app.mjs
+++ app.mjs
@@ -1,10 +1,23 @@
 import express from 'express';
+import mongoose from 'mongoose'
+import 'dotenv/config'
+
+import {router as itemsRouter} from './routes/items.mjs'
 
 const app = express();
+
+// Middlewares
+app.use('/items', itemsRouter)
 
 // Routes
 app.get('/', (req, res) => {
   res.send('We are home')
 })
+
+
+// Connect to db
+mongoose.connect(
+  process.env.MONGO_DB, () => console.log('connected to db')
+)
 
 app.listen(3001)
